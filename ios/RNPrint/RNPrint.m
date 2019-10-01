c49537e674723804cff3cb1a71334815381300d7
Jérémy M <jeremy.magrin@epitech.eu>
2019-10-01 18:40:46 +0200

    Get data from url asynchronously (#64)

--- ios/RNPrint/RNPrint.m
+++ ios/RNPrint/RNPrint.m
@@ -14,10 +14,60 @@
 
 RCT_EXPORT_MODULE();
 
+-(void)launchPrint:(NSData *) data
+        resolver:(RCTPromiseResolveBlock)resolve
+        rejecter:(RCTPromiseRejectBlock)reject {
+    if(!_htmlString && ![UIPrintInteractionController canPrintData:data]) {
+        reject(RCTErrorUnspecified, nil, RCTErrorWithMessage(@"Unable to print this filePath"));
+        return;
+    }
+    
+    UIPrintInteractionController *printInteractionController = [UIPrintInteractionController sharedPrintController];
+    printInteractionController.delegate = self;
+    
+    // Create printing info
+    UIPrintInfo *printInfo = [UIPrintInfo printInfo];
+    
+    printInfo.outputType = UIPrintInfoOutputGeneral;
+    printInfo.jobName = [_filePath lastPathComponent];
+    printInfo.duplex = UIPrintInfoDuplexLongEdge;
+    printInfo.orientation = _isLandscape? UIPrintInfoOrientationLandscape: UIPrintInfoOrientationPortrait;
+    
+    printInteractionController.printInfo = printInfo;
+    printInteractionController.showsPageRange = YES;
+    
+    if (_htmlString) {
+        UIMarkupTextPrintFormatter *formatter = [[UIMarkupTextPrintFormatter alloc] initWithMarkupText:_htmlString];
+        printInteractionController.printFormatter = formatter;
+    } else {
+        printInteractionController.printingItem = data;
+    }
+    
+    // Completion handler
+    void (^completionHandler)(UIPrintInteractionController *, BOOL, NSError *) =
+    ^(UIPrintInteractionController *printController, BOOL completed, NSError *error) {
+        if (!completed && error) {
+            NSLog(@"Printing could not complete because of error: %@", error);
+            reject(RCTErrorUnspecified, nil, RCTErrorWithMessage(error.description));
+        } else {
+            resolve(completed ? printInfo.jobName : nil);
+        }
+    };
+    
+    if (_pickedPrinter) {
+        [printInteractionController printToPrinter:_pickedPrinter completionHandler:completionHandler];
+    } else if([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) { // iPad
+        UIView *view = [[UIApplication sharedApplication] keyWindow].rootViewController.view;
+        [printInteractionController presentFromRect:view.frame inView:view animated:YES completionHandler:completionHandler];
+    } else { // iPhone
+        [printInteractionController presentAnimated:YES completionHandler:completionHandler];
+    }
+}
+
 RCT_EXPORT_METHOD(print:(NSDictionary *)options
                   resolver:(RCTPromiseResolveBlock)resolve
                   rejecter:(RCTPromiseRejectBlock)reject) {
-
+    
     if (options[@"filePath"]){
         _filePath = [RCTConvert NSString:options[@"filePath"]];
     } else {
@@ -43,66 +93,25 @@ RCT_EXPORT_METHOD(print:(NSDictionary *)options
         reject(RCTErrorUnspecified, nil, RCTErrorWithMessage(@"Must provide either `html` or `filePath`. Both are either missing or passed together"));
     }
     
-    NSData *printData;
+    __block NSData *printData;
     BOOL isValidURL = NO;
     NSURL *candidateURL = [NSURL URLWithString: _filePath];
     if (candidateURL && candidateURL.scheme && candidateURL.host)
         isValidURL = YES;
     
     if (isValidURL) {
-        // TODO: This needs updated to use NSURLSession dataTaskWithURL:completionHandler:
-        printData = [NSData dataWithContentsOfURL:candidateURL];
+        NSURLSession *session = [NSURLSession sharedSession];
+        NSURLSessionDataTask *dataTask = [session dataTaskWithURL:[NSURL URLWithString:_filePath] completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [self launchPrint:data resolver:resolve rejecter:reject];
+            });
+        }];
+        [dataTask resume];
     } else {
         printData = [NSData dataWithContentsOfFile: _filePath];
-    }
-    
-    if(!_htmlString && ![UIPrintInteractionController canPrintData:printData]) {
-        reject(RCTErrorUnspecified, nil, RCTErrorWithMessage(@"Unable to print this filePath"));
-        return;
-    }
-    
-    UIPrintInteractionController *printInteractionController = [UIPrintInteractionController sharedPrintController];
-    printInteractionController.delegate = self;
-    
-    // Create printing info
-    UIPrintInfo *printInfo = [UIPrintInfo printInfo];
-    
-    printInfo.outputType = UIPrintInfoOutputGeneral;
-    printInfo.jobName = [_filePath lastPathComponent];
-    printInfo.duplex = UIPrintInfoDuplexLongEdge;
-    printInfo.orientation = _isLandscape? UIPrintInfoOrientationLandscape: UIPrintInfoOrientationPortrait;
-    
-    printInteractionController.printInfo = printInfo;
-    printInteractionController.showsPageRange = YES;
-    
-    if (_htmlString) {
-        UIMarkupTextPrintFormatter *formatter = [[UIMarkupTextPrintFormatter alloc] initWithMarkupText:_htmlString];
-        printInteractionController.printFormatter = formatter;
-    } else {
-        printInteractionController.printingItem = printData;
-    }
-    
-    // Completion handler
-    void (^completionHandler)(UIPrintInteractionController *, BOOL, NSError *) =
-    ^(UIPrintInteractionController *printController, BOOL completed, NSError *error) {
-        if (!completed && error) {
-            NSLog(@"Printing could not complete because of error: %@", error);
-            reject(RCTErrorUnspecified, nil, RCTErrorWithMessage(error.description));
-        } else {
-            resolve(completed ? printInfo.jobName : nil);
-        }
-    };
-    
-    if (_pickedPrinter) {
-        [printInteractionController printToPrinter:_pickedPrinter completionHandler:completionHandler];
-    } else if([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) { // iPad
-        UIView *view = [[UIApplication sharedApplication] keyWindow].rootViewController.view;
-        [printInteractionController presentFromRect:view.frame inView:view animated:YES completionHandler:completionHandler];
-    } else { // iPhone
-        [printInteractionController presentAnimated:YES completionHandler:completionHandler];
+        [self launchPrint:printData resolver:resolve rejecter:reject];
     }
 }
-
 
 RCT_EXPORT_METHOD(selectPrinter:(NSDictionary *)options
                   resolver:(RCTPromiseResolveBlock)resolve
