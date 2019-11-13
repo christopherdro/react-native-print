ef36d7a9b7cb73e0d7f76c2202bdb31e4bd528b9
Johnathan Harms <johnathan@invoicesimple.com>
2019-11-13 12:12:07 -0800

    getString

--- android/src/main/java/com/christopherdro/RNPrint/RNPrintModule.java
+++ android/src/main/java/com/christopherdro/RNPrint/RNPrintModule.java
@@ -56,7 +56,7 @@ public class RNPrintModule extends ReactContextBaseJavaModule {
         final String html = options.hasKey("html") ? options.getString("html") : null;
         final String filePath = options.hasKey("filePath") ? options.getString("filePath") : null;
         final boolean isLandscape = options.hasKey("isLandscape") ? options.getBoolean("isLandscape") : false;
-        final String jobName = options.hasKey("jobName") ? options.getBoolean("jobName") : defaultJobName;
+        final String jobName = options.hasKey("jobName") ? options.getString("jobName") : defaultJobName;
 
         if ((html == null && filePath == null) || (html != null && filePath != null)) {
             promise.reject(getName(), "Must provide either `html` or `filePath`.  Both are either missing or passed together");
