22ce4b1f07ccb1dc7fe75c7060ae1558f24c3b94
Jaime Bernardo <jaime@janeasystems.com>
2020-10-13 15:33:07 -0700

    Add windows test to example

---
+++ example/__tests__/ShowPrintUI.test.js
@@ -0,0 +1,26 @@
+import { driver, By2 } from 'selenium-appium'
+import { until } from 'selenium-webdriver';
+
+const setup = require('../jest-setups/jest.setup');
+jest.setTimeout(60000);
+
+beforeAll(() => {
+  return driver.startWithCapabilities(setup.capabilites);
+});
+
+afterAll(() => {
+  return driver.quit();
+});
+
+describe('Test App', () => {
+
+  test('Opens Print UI', async () => {
+    await driver.wait(until.elementLocated(By2.nativeName('Print Remote PDF')));
+    (await driver.findElement(By2.nativeName('Print Remote PDF'))).click();
+    await driver.wait(until.elementLocated(By2.nativeName('Document - Print')));
+    (await driver.findElement(By2.nativeName('Cancel'))).click();
+    // Wait for print popup to close.
+    await driver.sleep(2000);
+  });
+
+})
